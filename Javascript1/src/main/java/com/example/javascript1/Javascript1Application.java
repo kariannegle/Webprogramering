c52e13e821a0368c6412e9c03b103504deb5bf6e
Karianne Glent <113008581+kariannegle@users.noreply.github.com>
2024-10-15 00:14:10 +0200

    javascript 1

---
+++ Javascript1/src/main/java/com/example/javascript1/Javascript1Application.java
@@ -0,0 +1,13 @@
+package com.example.javascript1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javascript1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javascript1Application.class, args);
+    }
+
+}
---
+++ Javascript1/src/test/java/com/example/javascript1/Javascript1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascript1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javascript1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
